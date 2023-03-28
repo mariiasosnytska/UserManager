b0eb40281d0e2fe758df14bff54d82dc66527f47
Mariia Sosnytska <mariia.sosnytskaua@gmail.com>
2023-03-28 18:17:48 +0300

    edited

--- src/main/java/com/example/UserManager/controller/UserController.java
+++ src/main/java/com/example/UserManager/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.example.UserManager.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.UserManager.model.UserDTO;
 import com.example.UserManager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -17,12 +17,27 @@ public class UserController {
     private UserRepository userRepo;
 
     @PostMapping("/addUser")
-    public UserDTO addUser(@RequestBody UserDTO user) {
-        return userRepo.save(user);
+    public ResponseEntity<?> addUser(@RequestBody UserDTO user) {
+        try{
+            userRepo.save(user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/getAllUser")
-    public List<UserDTO> getAllUser(){
-        return userRepo.findAll();
+    public ResponseEntity<?> getAllUsers() {
+        List<UserDTO> users = userRepo.findAll();
+        if(users.size() > 0) return new ResponseEntity<List<UserDTO>>(users, HttpStatus.OK);
+        else return new ResponseEntity<>("No users found", HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/getUser/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable("id") String id){
+        Optional<UserDTO> userOptional = userRepo.findById(id);
+        if (userOptional.isPresent()) return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
+        else return new ResponseEntity<>("Sorry, user with id " + id + "not found", HttpStatus.NOT_FOUND);
     }
 }
